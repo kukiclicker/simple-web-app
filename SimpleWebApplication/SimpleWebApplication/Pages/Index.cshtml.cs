b125f637d3a830cbc71c865c97a3887352ee949c
Uros Mitrovic <umitrovic22@gmail.com>
2023-06-09 01:42:09 +0200

    S3 bucket item added to home page

--- SimpleWebApplication/SimpleWebApplication/Pages/Index.cshtml.cs
+++ SimpleWebApplication/SimpleWebApplication/Pages/Index.cshtml.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Amazon.Runtime.Internal;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleWebApplication.Pages.Clients;
 
 namespace SimpleWebApplication.Pages
 {
     public class IndexModel : PageModel
     {
+
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -14,7 +19,7 @@ namespace SimpleWebApplication.Pages
 
         public void OnGet()
         {
-
+            
         }
     }
 }
